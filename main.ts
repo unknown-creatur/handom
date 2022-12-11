64f6160b589def0f707193bcecbcc5a823e5403a
421385 <115143096+unknown-creatur@users.noreply.github.com>
2022-12-11 20:39:04 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,32 @@
-let _0 = 0
-let _1 = 0
-let list: number[] = []
-let n = 0
-let a = 0
-let x = 0
-let y = 0
 input.onButtonPressed(Button.A, function () {
+    亮燈數 += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    亮燈數 = 0
+})
+input.onButtonPressed(Button.B, function () {
+    亮燈數 += -1
+})
+function 數 (num: number) {
+    if (num % 5 == 0) {
+        x = 4
+        y = Math.floor(num / 5) - 1
+    } else {
+        x = num % 5 - 1
+        y = Math.floor(num / 5)
+    }
+    led.plot(x, y)
+}
+let a = 0
+let n = 0
+let list: number[] = []
+let y = 0
+let x = 0
+let 亮燈數 = 0
+let _1 = 0
+let _0 = 0
+亮燈數 = 0
+basic.forever(function () {
     basic.clearScreen()
     _0 = 0
     _1 = 1
@@ -14,7 +35,7 @@ input.onButtonPressed(Button.A, function () {
         list.push(_0)
     }
     n = 0
-    while (n < 3) {
+    while (n < 亮燈數) {
         a = randint(0, 24)
         if (list[a] == 0) {
             list[a] = _1
@@ -22,14 +43,5 @@ input.onButtonPressed(Button.A, function () {
             數(a + 1)
         }
     }
+    basic.pause(2.5)
 })
-function 數 (num: number) {
-    if (num % 5 == 0) {
-        x = 4
-        y = Math.floor(num / 5) - 1
-    } else {
-        x = num / 5 - 1
-        y = Math.floor(num / 5)
-    }
-    led.plot(x, y)
-}
